045ef6a41b5275734c05e94e6d6350cec195e67b
slotwek <slotwek@gmail.com>
2024-04-21 21:15:12 +0100

    Made the script prittier

--- bash/script.sh
+++ bash/script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-yt-dlp -o "./%(playlist)s/2%(playlist_index)02d_%(title)s.%(ext)s"  -x --audio-format mp3  https://www.youtube.com/playlist?list=PLzMffrYfWh46v9uXukWe4KpCwgi41y1rC  --download-archive archive.txt
+yt-dlp -o "./%(playlist)s/3%(playlist_index)02d_%(title)s.%(ext)s"  \
+	-x --audio-format mp3 \
+	${1} \
+	--download-archive archive_como_aprender.txt
