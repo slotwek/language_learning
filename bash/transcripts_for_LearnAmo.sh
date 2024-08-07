a539ff90a03082137a611575bf9d7a7b911b4dda
slotwek <slotwek@gmail.com>
2024-08-07 10:59:33 +0100

    vault backup: 2024-08-07 10:59:33

--- bash/transcripts_for_LearnAmo.sh
+++ bash/transcripts_for_LearnAmo.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Get subtitles
-yt-dlp --skip-download \
-	--write-sub --write-auto-subs --sub-lang ${1} --sub-format vtt ${2} \
-	-o "./%(playlist)s/%(playlist_index)03d_%(title)s.%(ext)s" \
-	--download-archive download_archive.txt \
-	--match-filter "duration > 600"
+# yt-dlp --skip-download \
+#     --write-sub --write-auto-subs --sub-lang ${1} --sub-format vtt ${2} \
+#     -o "./%(playlist)s/%(playlist_index)03d_%(title)s.%(ext)s" \
+#     --download-archive download_archive.txt \
+#     --match-filter "duration > 600"
 
-# sed -e '/^[0-9][0-9]:..*/d' -e '/^$/d' -e '/^\[..*\]$/d' transcript.it.vtt
+for n in LearnAmo_raw/*.vtt; do
+    echo $n
+    sed -e '/^[0-9][0-9]:..*/d' -e '/^$/d' -e '/^\[..*\]$/d' ${n} > ${n}.txt
+done
