71e7220bb8c899727cb4adaeb039c301e26de05f
slotwek <slotwek@gmail.com>
2024-08-07 10:04:05 +0100

    vault backup: 2024-08-07 10:04:05

--- bash/transcripts_for_LearnAmo.sh
+++ bash/transcripts_for_LearnAmo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Get subtitles
-yt-dlp --skip-download --write-sub --write-auto-subs --sub-lang ${1} --sub-format vtt ${2} -o "transcript"
+yt-dlp --skip-download --write-sub --write-auto-subs --sub-lang ${1} --sub-format vtt ${2} -o "./%(playlist)s/%(playlist_index)03d_%(title)s.%(ext)s" --download-archive download_archive.txt
 
-sed -e '/^[0-9][0-9]:..*/d' -e '/^$/d' -e '/^\[..*\]$/d' transcript.it.vtt
+# sed -e '/^[0-9][0-9]:..*/d' -e '/^$/d' -e '/^\[..*\]$/d' transcript.it.vtt
